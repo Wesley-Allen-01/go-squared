adfaa38c450472f9cc4c146d0b99e1d19b2b6977
Wesley Allen <weal2937@colorado.edu>
2026-06-05 19:06:42 -0600

    finishing up captures

--- game/capture.go
+++ game/capture.go
@@ -1,8 +1,22 @@
 package game
 
 func RemoveGroup(b *Board, group []Point) {
+	for _, pt := range group {
+		b.Set(pt, Empty)
+	}
 }
 
 func CheckCaptures(b *Board, p Point, color Color) int {
-	return 0
+	captures := 0
+	for _, neighbor := range b.getNeighbors(p) {
+		if b.Get(neighbor) == color {
+			continue
+		}
+		neighborGroup := FindGroup(b, neighbor)
+		if IsCaptured(b, neighborGroup) {
+			captures += len(neighborGroup)
+			RemoveGroup(b, neighborGroup)
+		}
+	}
+	return captures
 }
